7ffeb569ab02ab83d784a4fb5a2d55cce95e07e4
sammish93 <samd93@hotmail.co.uk>
2022-09-25 18:45:09 +0200

    testing git push

--- Java_Project/src/main/java/no/hiof/groupproject/Main.java
+++ Java_Project/src/main/java/no/hiof/groupproject/Main.java
@@ -5,6 +5,8 @@ import no.hiof.groupproject.tools.ExampleToolClass;
 public class Main {
     public static void main(String[] args) {
 
+        //Test comment for a git test
+
         ExampleToolClass helloBot = new ExampleToolClass();
         System.out.println(helloBot.returnHello());
 
