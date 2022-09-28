c7064a4e4d0385884b2d5b7a91ccec7f8cb69504
a5h71 <ashtim@hiof.no>
2022-09-28 12:02:52 +0200

    change instance variables

--- Java_Project/src/main/java/no/hiof/groupproject/models/Camper.java
+++ Java_Project/src/main/java/no/hiof/groupproject/models/Camper.java
@@ -2,8 +2,10 @@ package no.hiof.groupproject.models;
 
 
 public class Camper extends Vehicle {
-    private int seatingCapacity;
-
+    private int seatingCapacity, towingAbility;
+    private boolean includesToilet, includesBed, includesKitchen;
+    /* check if the camper includes the above if true
+    * there should be additional rates */
     public Camper(){
         super();
     }
