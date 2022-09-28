1896e78f08ba31885f3a72ace20fbc0b86fbe18a
a5h71 <ashtim@hiof.no>
2022-09-28 11:38:15 +0200

    change instance variable

--- Java_Project/src/main/java/no/hiof/groupproject/models/Vehicle.java
+++ Java_Project/src/main/java/no/hiof/groupproject/models/Vehicle.java
@@ -4,7 +4,7 @@ public abstract class Vehicle {
     /* update the necessary instance variable needed */
     private String vehicle_id, regNo, manufacturer, model, fuel_type, gearbox;
     private int modelYear, price;
-    private Owner owner;
+    private Owner owner; //
     private int vehicleStatus; // availability , rented or not
 
     // private ArrayList<RentalRecord> records= new ArrayList<>(); --> not sure where to place this,if under person class or Vehicle class
