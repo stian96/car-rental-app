a2eb307eabc2f1c78179a8def50afa28adbfa94b
a5h71 <ashtim@hiof.no>
2022-09-30 12:48:20 +0200

    Updated new

--- Java_Project/src/main/java/no/hiof/groupproject/models/Person.java
+++ Java_Project/src/main/java/no/hiof/groupproject/models/Person.java
@@ -3,7 +3,7 @@ package no.hiof.groupproject.models;
 public abstract class Person{
 
     private int id,dLicense, phone;
-    private String first_name, last_name, address, email;
+    private String first_name, last_name, bankAccountNum, address, email;
 
 
 
