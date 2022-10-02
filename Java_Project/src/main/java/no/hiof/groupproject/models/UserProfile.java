57d171c44cbbe4b8c970cdf283c3f4d5ed286b79
a5h71 <ashtim@hiof.no>
2022-10-02 13:53:12 +0200

    new userprofile class

---
+++ Java_Project/src/main/java/no/hiof/groupproject/models/UserProfile.java
@@ -0,0 +1,7 @@
+package no.hiof.groupproject.models;
+
+public class UserProfile {
+
+    //Not sure if this class is needed since it would contain the person class which we already have
+    private Person person;
+}
