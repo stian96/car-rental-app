c57976107399b104b1b6496f9c6b360aa3ed368f
rachel <rchlmd83@gmail.com>
2022-10-13 09:19:57 +0200

    update

--- Java_Project/src/main/java/no/hiof/groupproject/Controller/Main.java
+++ Java_Project/src/main/java/no/hiof/groupproject/Controller/Main.java
@@ -1,2 +1,37 @@
-package no.hiof.groupproject.Controller;public class Main {
+package no.hiof.groupproject.Controller;
+
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+public class Main extends Application{
+
+    private static Stage stg;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        stg = stage;
+        stage.setResizable(false);
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("LogIn.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+        stage.setTitle("ToGoCar");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    public void changeScene(String fxml) throws IOException{
+        Parent pane;
+        pane = FXMLLoader.load(getClass().getResource(fxml));
+        stg.getScene().setRoot(pane);
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
 }
--- Java_Project/src/main/java/no/hiof/groupproject/Controller/SignUpController.java
+++ Java_Project/src/main/java/no/hiof/groupproject/Controller/SignUpController.java
@@ -1,2 +1,133 @@
-package no.hiof.groupproject.Controller;public class SignUpController {
+package no.hiof.groupproject.Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Label;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class SignUpController {
+
+    @FXML
+    private TextField tf_userName;
+    @FXML
+    private PasswordField tf_password;
+    @FXML
+    private Button button_login;
+    @FXML
+    private Button button_signUp;
+    @FXML
+    private Button button_signUpGoogle;
+    @FXML
+    private Button button_signUpFacebook;
+    @FXML
+    private Label validEmail;
+
+
+
+
+    public void userLogIn(ActionEvent event) throws IOException {
+        LogInCheck();
+    }
+
+    public void userSignUp(ActionEvent event) throws IOException{
+        SignUpCheck();
+    }
+
+    public void signUpGoogle(ActionEvent event) throws IOException {
+        SignUpCheckGoogle();
+    }
+
+    public void signUpFacebook(ActionEvent event) throws IOException{
+        SignUpCheckFacebook();
+    }
+
+    private void SignUpCheck() throws IOException {
+        Main m = new Main();
+
+        HashMap<String, String> userInfo = new HashMap<String, String>();
+        userInfo.put("John", "cool");
+        userInfo.put("John1", "cool1");
+        userInfo.put("John2", "cool2");
+        userInfo.put("John3", "cool3");
+        userInfo.put("John4", "cool4");
+
+        for (Map.Entry<String, String> entry : userInfo.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (tf_userName.getText().toString().equals(key) && tf_password.getText().toString().equals(value)) {
+                validEmail.setText("Success!");
+                m.changeScene("ToGoCar.fxml");
+            }else if (tf_userName.getText().isEmpty() && tf_password.getText().isEmpty()) {
+                validEmail.setText("Please enter Email and Password.");
+            }
+            else {
+                validEmail.setText("Wrong email or password");
+            }
+
+        }
+
+    }
+
+    private void SignUpCheckGoogle() throws IOException {
+        Main m = new Main();
+
+        HashMap<String, String> userInfo = new HashMap<String, String>();
+        userInfo.put("John@gmail.com", "cool");
+        userInfo.put("John1@gmail.com", "cool1");
+        userInfo.put("John2@gmail.com", "cool2");
+        userInfo.put("John3@gmail.com", "cool3");
+        userInfo.put("John4@gmail.com", "cool4");
+
+        for (Map.Entry<String, String> entry : userInfo.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (tf_userName.getText().toString().equals(key) && tf_password.getText().toString().equals(value)) {
+                validEmail.setText("Success!");
+                m.changeScene("ToGoCar.fxml");
+            }else if (tf_userName.getText().isEmpty() && tf_password.getText().isEmpty()) {
+                validEmail.setText("Please enter Email and Password.");
+            }
+            else {
+                validEmail.setText("Invalid username");
+            }
+
+        }
+    }
+    private void SignUpCheckFacebook() throws IOException {
+        Main m = new Main();
+        HashMap<String, String> userInfo = new HashMap<String, String>();
+        userInfo.put("John@gmail.com", "cool");
+        userInfo.put("John1@gmail.com", "cool1");
+        userInfo.put("John2@gmail.com", "cool2");
+        userInfo.put("John3@gmail.com", "cool3");
+        userInfo.put("John4@gmail.com", "cool4");
+
+        for (Map.Entry<String, String> entry : userInfo.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            if (tf_userName.getText().toString().equals(key) && tf_password.getText().toString().equals(value)) {
+                validEmail.setText("Success!");
+                m.changeScene("ToGoCar.fxml");
+            }else if (tf_userName.getText().isEmpty() && tf_password.getText().isEmpty()) {
+                validEmail.setText("Please enter Email and Password.");
+            }
+            else {
+                validEmail.setText("Wrong email or password");
+            }
+
+        }
+    }
+
+    private void LogInCheck() throws IOException{
+        Main m = new Main();
+        m.changeScene("logIn.fxml");
+
+    }
 }
