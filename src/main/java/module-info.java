afe517edcebdc3dd57e36065dbdf2a00789f66bc
sammish93 <samd93@hotmail.co.uk>
2022-10-13 14:33:36 +0200

    deleted old intellij folders

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.java_project {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.java_project to javafx.fxml;
-    exports com.example.java_project;
-}
