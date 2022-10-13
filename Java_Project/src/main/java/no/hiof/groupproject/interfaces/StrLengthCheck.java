5cd512a3f2bf0db7620677964bea4cf9241a4452
stian96 <stiankru@hiof.no>
2022-10-13 13:20:33 +0200

    Java_Project is deleted

--- Java_Project/src/main/java/no/hiof/groupproject/interfaces/StrLengthCheck.java
+++
@@ -1,9 +0,0 @@
-package no.hiof.groupproject.interfaces;
-
-//easy default method interface to return a boolean value
-//compares string length to desired length and returns true if they match
-public interface StrLengthCheck {
-    default boolean checkLength(String item, int length) {
-        return item.length() == length;
-    }
-}
