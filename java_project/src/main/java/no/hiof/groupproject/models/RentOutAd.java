c6fe44784221fa1fe8f6aeb0fa0c3384060a6e55
sammish93 <samd93@hotmail.co.uk>
2022-10-16 16:10:29 +0200

    added functionality to ensure bookings are valid only if dates are available and no other bookings clash

--- java_project/src/main/java/no/hiof/groupproject/models/RentOutAd.java
+++ java_project/src/main/java/no/hiof/groupproject/models/RentOutAd.java
@@ -1,12 +1,8 @@
 package no.hiof.groupproject.models;
 
-import no.hiof.groupproject.models.payment_methods.Payment;
-
 import java.time.LocalDate;
 import java.time.Period;
-import java.util.ArrayList;
-import java.util.Currency;
-import java.util.HashMap;
+import java.util.*;
 
 /*
 A subclass of Advertisement designed to be used for users wanting to rent out their car.
@@ -17,8 +13,9 @@ public class RentOutAd extends Advertisement {
 
     private Vehicle vehicle;
     private Currency nok;
-    //hashmap of the time that the vehicle is available to be rented within
-    private HashMap<LocalDate, LocalDate> availableWithin;
+    //treemap of the time that the vehicle is available to be rented within
+    //sorted array from oldest -> newest date based on the key
+    private TreeMap<LocalDate, LocalDate> availableWithin;
     //array of confirmed bookings connected to a singular advertisement
     private ArrayList<Booking> confirmedBookings;
 
@@ -35,17 +32,68 @@ public class RentOutAd extends Advertisement {
         availableWithin.put(dateFrom, dateTo);
     }
 
-    //function to add a new booking
+    //function to add a new booking, ensuring the date is available, and no other booking happens at the same time
     public void addBooking(Booking booking) {
-        for (HashMap.Entry<LocalDate, LocalDate> set : availableWithin.entrySet()) {
+
+        boolean dateAvailable = false;
+        boolean dateDoesNotClash = false;
+
+        for (Map.Entry<LocalDate, LocalDate> set : availableWithin.entrySet()) {
+            //as long as the booking is within the available dates then the code can proceed
             if (booking.getBookedFrom().isAfter(set.getKey()) && booking.getBookedTo().isBefore(set.getValue())) {
-                //creates a booking in the format of <renter id>.<date booking begins>.<vehicle owner id>
-                //42.2024-12-24.26
-                booking.setStrId(booking.getStrId() + "." + this.getUser().getId());
-                confirmedBookings.add(booking);
+                dateAvailable = true;
             }
         }
 
+        if (dateAvailable) {
+            //creating a sorted map of bookings
+            Map<LocalDate, LocalDate> sortedBookings = new TreeMap<>();
+
+            //initialising variables
+            LocalDate dateTo = null;
+            LocalDate dateFrom = null;
+
+            //flags for the following for loop
+            boolean iteration = true;
+            boolean turn = false;
+
+            //TLDR - establishes periods between bookings when nobody is booking the vehicle.
+
+            //on first iteration the booking's last date is set to a variable then the turn flag is set to true
+            //    on second iteration the booking's first date is set to a variable, turn flag is set to false,
+            //    then a set is put into the TreeMap, and the turn flag is set to true.
+            //        on the third iteration the loop is repeated
+            for (Booking book : confirmedBookings) {
+                if (iteration) {
+                    dateTo = book.getBookedTo();
+                    iteration = false;
+                } else {
+                    dateFrom = book.getBookedFrom();
+                    iteration = true;
+                }
+                if (turn && dateTo != null && dateFrom != null) {
+                    sortedBookings.put(dateTo, dateFrom);
+                    turn = false;
+                } else {
+                    turn = true;
+                }
+            }
+            //sets a flag if the booking does not clash with another booking
+            for (Map.Entry<LocalDate, LocalDate> set : sortedBookings.entrySet()) {
+                if (booking.getBookedFrom().isAfter(set.getKey()) && booking.getBookedTo().isAfter(set.getValue())) {
+                    dateDoesNotClash = true;
+                }
+            }
+        }
+
+        //if and ONLY IF the booking date is within dates available AND does not clash with other booking dates
+        //then the booking will be added to a booking array
+        if (dateDoesNotClash) {
+            //creates a booking in the format of <renter id>.<date booking begins>.<vehicle owner id>
+            //42.2024-12-24.26
+            booking.setStrId(booking.getStrId() + "." + this.getUser().getId());
+            confirmedBookings.add(booking);
+        }
     }
 
     //function used to remove rental periods that are now expired
@@ -71,11 +119,11 @@ public class RentOutAd extends Advertisement {
         this.nok = nok;
     }
 
-    public HashMap<LocalDate, LocalDate> getAvailableWithin() {
+    public TreeMap<LocalDate, LocalDate> getAvailableWithin() {
         return availableWithin;
     }
 
-    public void setAvailableWithin(HashMap<LocalDate, LocalDate> availableWithin) {
+    public void setAvailableWithin(TreeMap<LocalDate, LocalDate> availableWithin) {
         this.availableWithin = availableWithin;
     }
 
@@ -87,3 +135,5 @@ public class RentOutAd extends Advertisement {
         this.confirmedBookings = confirmedBookings;
     }
 }
+
+
