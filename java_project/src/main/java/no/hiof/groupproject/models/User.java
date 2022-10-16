fc265612d7ef53b1bf67bf2d6c66349a16a14041
sammish93 <samd93@hotmail.co.uk>
2022-10-16 11:28:49 +0200

    redefined User class variables

--- java_project/src/main/java/no/hiof/groupproject/models/User.java
+++ java_project/src/main/java/no/hiof/groupproject/models/User.java
@@ -2,11 +2,19 @@ package no.hiof.groupproject.models;
 
 public class User {
 
-    private int id,dLicense;
+    private int id;
+
+    //integration of a driving license class here would be great - so that VerifyDrivingLicense could be used when
+    //a GUI button to rent a car is activated
+    //private DrivingLicence dLicense:
+
+
     private String firstName, lastName, postNr;
+    //obviously the password would be encrypted in a final build
+    private String password;
     //relevant for receiving payment via card, paypal, vipps etc.
     private String bankAccountNr, email, tlfNr;
-    //possible to replace address with
+
 
 
 }
