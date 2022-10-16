a54ad00031652aaa852bb21139cb6b7e30c812e6
stian96 <stiankru@hiof.no>
2022-10-16 15:46:46 +0200

    "Added constructor and getters/setters in Vehicle and Car class"

--- java_project/src/main/java/no/hiof/groupproject/models/Car.java
+++ java_project/src/main/java/no/hiof/groupproject/models/Car.java
@@ -4,7 +4,39 @@ public class Car extends Vehicle{
     private int seatingCapacity;
     private int towingAbility;
     private boolean canTow; /*if the car has trailer attachment. If true then how much to tow*/
-    public Car(){
-        super();
+
+
+    public Car(String ID, String regNumber, String manufacturer, String model, String fuelType, String gearBox,
+               int modelYear, int price, User owner, int vehicleStatus, int seatingCapacity, int towingAbility,
+               boolean canTow) {
+
+        super(ID, regNumber, manufacturer, model, fuelType, gearBox, modelYear, price, owner, vehicleStatus);
+        this.seatingCapacity = seatingCapacity;
+        this.towingAbility = towingAbility;
+        this.canTow = canTow;
+    }
+
+    public int getSeatingCapacity() {
+        return seatingCapacity;
+    }
+
+    public void setSeatingCapacity(int seatingCapacity) {
+        this.seatingCapacity = seatingCapacity;
+    }
+
+    public int getTowingAbility() {
+        return towingAbility;
+    }
+
+    public void setTowingAbility(int towingAbility) {
+        this.towingAbility = towingAbility;
+    }
+
+    public boolean isCanTow() {
+        return canTow;
+    }
+
+    public void setCanTow(boolean canTow) {
+        this.canTow = canTow;
     }
 }
--- java_project/src/main/java/no/hiof/groupproject/models/Vehicle.java
+++ java_project/src/main/java/no/hiof/groupproject/models/Vehicle.java
@@ -4,11 +4,105 @@ public abstract class Vehicle {
     /* update the necessary instance variable needed */
     private String vehicle_id, regNo, manufacturer, model, fuel_type, gearbox;
     private int modelYear, price;
-    private User owner; //
-    private int vehicleStatus; // availability , rented or not
+    private User owner;
+    private int vehicleStatus;
 
-    // private ArrayList<RentalRecord> records= new ArrayList<>(); --> not sure where to place this,if under person class or Vehicle class
+    // Default constructor.
+    public Vehicle() {
 
+    }
 
+    public Vehicle(String ID, String regNumber, String manufacturer, String model, String fuelType,
+                   String gearBox, int modelYear, int price, User owner, int vehicleStatus) {
+        this.vehicle_id = ID;
+        this.regNo = regNumber;
+        this.manufacturer = manufacturer;
+        this.model = model;
+        this.fuel_type = fuelType;
+        this.gearbox = gearBox;
+        this.modelYear = modelYear;
+        this.price = price;
+        this.owner = owner;
+        this.vehicleStatus = vehicleStatus;
+    }
 
+    public String getVehicle_id() {
+        return vehicle_id;
+    }
+
+    public void setVehicle_id(String vehicle_id) {
+        this.vehicle_id = vehicle_id;
+    }
+
+    public String getRegNo() {
+        return regNo;
+    }
+
+    public void setRegNo(String regNo) {
+        this.regNo = regNo;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getFuel_type() {
+        return fuel_type;
+    }
+
+    public void setFuel_type(String fuel_type) {
+        this.fuel_type = fuel_type;
+    }
+
+    public String getGearbox() {
+        return gearbox;
+    }
+
+    public void setGearbox(String gearbox) {
+        this.gearbox = gearbox;
+    }
+
+    public int getModelYear() {
+        return modelYear;
+    }
+
+    public void setModelYear(int modelYear) {
+        this.modelYear = modelYear;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public int getVehicleStatus() {
+        return vehicleStatus;
+    }
+
+    public void setVehicleStatus(int vehicleStatus) {
+        this.vehicleStatus = vehicleStatus;
+    }
 }
