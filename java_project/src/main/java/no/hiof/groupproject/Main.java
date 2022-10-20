bf12b996725b5b5b04f5afd8c8c4544c4c57a9ed
stian96 <stiankru@hiof.no>
2022-10-20 14:24:05 +0200

    "Cleaned up Main class"

--- java_project/src/main/java/no/hiof/groupproject/Main.java
+++ java_project/src/main/java/no/hiof/groupproject/Main.java
@@ -1,11 +1,5 @@
 package no.hiof.groupproject;
 
-
-import com.fasterxml.jackson.databind.JsonNode;
-import no.hiof.groupproject.tools.geocode.Location;
-
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args)  {
 
