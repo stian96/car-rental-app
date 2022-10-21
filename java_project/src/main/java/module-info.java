bface709ba7655bf65d9920844e9a92d4dacc996
sammish93 <samd93@hotmail.co.uk>
2022-10-21 14:59:23 +0200

    integrated jakarta and hibernate into the project, as well as hibernate community dialect for SQLite

--- java_project/src/main/java/module-info.java
+++ java_project/src/main/java/module-info.java
@@ -4,7 +4,9 @@ module com.example.java_project {
     requires java.sql;
     requires com.fasterxml.jackson.core;
     requires com.fasterxml.jackson.databind;
-
+    requires jakarta.persistence;
+    requires org.hibernate.orm.core;
+    requires org.hibernate.orm.community.dialects;
 
     opens com.example.java_project to javafx.fxml;
     exports com.example.java_project;
--- java_project/src/main/java/no/hiof/groupproject/Main.java
+++ java_project/src/main/java/no/hiof/groupproject/Main.java
@@ -1,26 +1,23 @@
 package no.hiof.groupproject;
 
-import no.hiof.groupproject.models.Car;
-import no.hiof.groupproject.models.RentOutAd;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import no.hiof.groupproject.models.User;
-import no.hiof.groupproject.models.Vehicle;
 import no.hiof.groupproject.tools.db.GenericQueryDB;
 
+import java.io.File;
+import java.io.IOException;
 import java.math.BigDecimal;
 import java.sql.PreparedStatement;
 import java.util.Currency;
 
 public class Main {
-    public static void main(String[] args)  {
-
-        RentOutAd roa = new RentOutAd(
-                new User("Sam", "Davies", "1111", "hunter2",
-                        "123412341234", "sam@sam.no", "12345678"),
-                new Car(),
-                BigDecimal.valueOf(200), BigDecimal.valueOf(10)
-        );
+    public static void main(String[] args) {
 
         GenericQueryDB.query("INSERT INTO people(name,age) VALUES('jesper', 14)");
+
+        User user = new User("sam", "davies", "1111", "hunter2", "1234123412341234",
+                "sam@sam.no", "12341234");
 
     }
 }
