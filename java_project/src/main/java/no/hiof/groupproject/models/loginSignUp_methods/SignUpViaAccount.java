6eb7147ccb537480a9623a6991288bdcf4bddefb
sammish93 <samd93@hotmail.co.uk>
2022-10-31 08:43:26 +0100

    abstracted SignUpFacebook/Google to SignUpViaAccount

---
+++ java_project/src/main/java/no/hiof/groupproject/models/loginSignUp_methods/SignUpViaAccount.java
@@ -0,0 +1,11 @@
+package no.hiof.groupproject.models.loginSignUp_methods;
+
+public abstract class SignUpViaAccount extends Registration{
+    public SignUpViaAccount(String email, String password) {
+        super(email, password);
+    }
+
+    public SignUpViaAccount() {
+
+    }
+}
