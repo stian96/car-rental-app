5bcbece73c357af13f361334c2884511781761b6
a5h71 <ashtim@hiof.no>
2022-11-05 10:43:12 +0100

    adding changes to logInController

--- java_project/src/main/java/com/example/java_project/LogInController.java
+++ java_project/src/main/java/com/example/java_project/LogInController.java
@@ -37,46 +37,22 @@ public class LogInController {
         User u = RetrieveUserDB.retrieveFromEmail(tf_userName.getText());
         String password = tf_password.getText();
         String email = tf_userName.getText();
-        if(u.getEmail().equals(email) && u.getPassword().equals(password)){
-            wrongLogin.setText("success");
-            m.changeScene("ToGoCar.fxml");
-        }
-        else if (tf_userName.getText().isEmpty() && tf_password.getText().isEmpty()) {
-            wrongLogin.setText("Please enter Email and Password.");
-        }
-        else {
-            wrongLogin.setText("Wrong email or password");
-        }
-    }
-
+        if(!email.isEmpty() && !password.isEmpty()){
+            try {
+                if(!u.existsInDb()){
+                    SignUpCheck();
+                }
+                else if(u.getEmail().equals(email) && u.getPassword().equals(password)){
+                    m.changeScene("ToGoCar.fxml");
+                }
+                else {wrongLogin.setText("Wrong email or password");}
+            }catch (IOException e){
+                System.out.println(e.getMessage());
+            }
+        }}
     public void userSignUp(ActionEvent event) throws IOException{
         SignUpCheck();
     }
-/*
-    public void isVerified() throws IOException {
-        Main m = new Main();
-
-        HashMap<String, String> loginverification = new HashMap<>();
-        loginverification.put("john1@gmail.com", "john1");
-        loginverification.put("john2@gmail.com", "john2");
-        loginverification.put("john3@gmail.com", "john3");
-        for (Map.Entry<String, String> entry : loginverification.entrySet()) {
-            String key = entry.getKey();
-            String value = entry.getValue();
-            if (tf_userName.getText().equals(key) && tf_password.getText().equals(value)) {
-                wrongLogin.setText("Success!");
-                m.changeScene("ToGoCar.fxml");
-            }else if (tf_userName.getText().isEmpty() && tf_password.getText().isEmpty()) {
-                wrongLogin.setText("Please enter Email and Password.");
-            }
-            else {
-                wrongLogin.setText("Wrong email or password");
-            }
-
-
-        }
-
-    }*/
     private void SignUpCheck() throws IOException {
         Main m = new Main();
         m.changeScene("SignUp.fxml");
