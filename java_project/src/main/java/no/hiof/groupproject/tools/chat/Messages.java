ae5d4dce4b0a2d80b11426ac045c8e9327140e42
stian96 <stiankru@hiof.no>
2022-11-06 22:09:03 +0100

    Updated stuff in Message class

--- java_project/src/main/java/no/hiof/groupproject/tools/chat/Messages.java
+++ java_project/src/main/java/no/hiof/groupproject/tools/chat/Messages.java
@@ -1,4 +1,17 @@
 package no.hiof.groupproject.tools.chat;
 
 public class Messages {
+    private String message;
+
+    public Messages(String msg) {
+        this.message = msg;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
