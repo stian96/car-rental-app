aed9a7dff8ca0a815e95aa764c0e9dedadd945e0
sammish93 <samd93@hotmail.co.uk>
2022-11-09 15:19:46 +0100

    added more database tests - testing table existence

--- java_project/src/test/java/no/hiof/groupproject/tools/db/DatabaseTest.java
+++ java_project/src/test/java/no/hiof/groupproject/tools/db/DatabaseTest.java
@@ -1,8 +1,14 @@
 package no.hiof.groupproject.tools.db;
 
+import no.hiof.groupproject.models.Advertisement;
+import no.hiof.groupproject.models.vehicle_types.Vehicle;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Objects;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -19,7 +25,104 @@ class DatabaseTest {
         assertTrue(f.exists());
     }
     @Test
-    void assertsConnectionEstablishedWithDatabase() {
+    void assertsTablesExistInDatabase() {
 
+        String sql = "SELECT * FROM sqlite_master GROUP BY tbl_name";
+
+        boolean ans = true;
+        // all table names in the database
+        String[] tblNames = {"advertisements", "availableWithin", "bookings", "licenses", "messages", "payments",
+                "ratings", "userProfiles", "users", "vehicles", "sqlite_master", "sqlite_sequence"};
+        try (Connection conn = ConnectDB.connect();
+             Statement str = conn.createStatement();
+             ResultSet rs = str.executeQuery(sql)) {
+
+            //checks to see if all tables exist in database
+            while (rs.next()) {
+                String table = rs.getString("tbl_name");
+                for (String s : tblNames) {
+                    if (!Arrays.asList(tblNames).contains(table)) {
+                        ans = false;
+                        break;
+                    }
+                    if (!ans) {
+                        break;
+                    }
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertTrue(ans);
     }
+
+    @Test
+    void assertsTablesExistInTestDatabase() {
+
+        String sql = "SELECT * FROM sqlite_master GROUP BY tbl_name";
+
+        boolean ans = true;
+        // all table names in the database
+        String[] tblNames = {"advertisements", "availableWithin", "bookings", "licenses", "messages", "payments",
+                "ratings", "userProfiles", "users", "vehicles", "sqlite_master", "sqlite_sequence"};
+        try (Connection conn = ConnectSpecifiedDB.connect("jdbc:sqlite:sqlite/db/testable.db");
+             Statement str = conn.createStatement();
+             ResultSet rs = str.executeQuery(sql)) {
+
+            //checks to see if all tables exist in database
+            while (rs.next()) {
+                String table = rs.getString("tbl_name");
+                for (String s : tblNames) {
+                    if (!Arrays.asList(tblNames).contains(table)) {
+                        ans = false;
+                        break;
+                    }
+                    if (!ans) {
+                        break;
+                    }
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertTrue(ans);
+    }
+
+    @Test
+    void assertsContrapositiveTableExistenceLogic() {
+
+        String sql = "SELECT * FROM sqlite_master GROUP BY tbl_name";
+
+        boolean ans = true;
+        // tblNames contains tables that aren't supposed to exist in the database
+        String[] tblNames = {"wrong", "table", "array"};
+        try (Connection conn = ConnectSpecifiedDB.connect("jdbc:sqlite:sqlite/db/testable.db");
+             Statement str = conn.createStatement();
+             ResultSet rs = str.executeQuery(sql)) {
+
+            //checks to see if all tables exist in database
+            while (rs.next()) {
+                String table = rs.getString("tbl_name");
+                for (String s : tblNames) {
+                    if (!Arrays.asList(tblNames).contains(table)) {
+                        ans = false;
+                        break;
+                    }
+                    if (!ans) {
+                        break;
+                    }
+                }
+
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertFalse(ans);
+    }
+
 }
