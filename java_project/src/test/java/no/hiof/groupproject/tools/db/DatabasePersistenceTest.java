555d132fb20464fd07b7a7323141238bf51aa02e
sammish93 <samd93@hotmail.co.uk>
2022-11-10 08:26:37 +0100

    added more user persistant saving tests

--- java_project/src/test/java/no/hiof/groupproject/tools/db/DatabasePersistenceTest.java
+++ java_project/src/test/java/no/hiof/groupproject/tools/db/DatabasePersistenceTest.java
@@ -6,10 +6,7 @@ import no.hiof.groupproject.models.UserProfile;
 import org.junit.jupiter.api.*;
 
 import java.io.File;
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 import java.time.LocalDate;
 import java.util.Arrays;
 
@@ -29,7 +26,7 @@ class DatabasePersistenceTest {
     @Test
     void assertsUserClassIsSerialised() {
         User user = new User("testing@this.email", "breadismyfavouritefood");
-        User user2 = RetrieveUserDB.retrieveFromEmail("testing@this.email");
+        User user2 = RetrieveUserDB.retrieveFromEmail(user.getEmail());
 
         assertEquals(user.getEmail(), user2.getEmail());
         assertEquals(user.getPassword(), user2.getPassword());
@@ -40,7 +37,7 @@ class DatabasePersistenceTest {
         User user = new User("test2@this.email", "butterismyfavouritefood");
         assertNull(user.getFirstName());
 
-        User user2 = RetrieveUserDB.retrieveFromEmail("test2@this.email");
+        User user2 = RetrieveUserDB.retrieveFromEmail(user.getEmail());
         assertNull(user2.getFirstName());
     }
 
@@ -55,7 +52,7 @@ class DatabasePersistenceTest {
                 new License("98 45 123456 1", LocalDate.parse("2008-05-12"),
                 "Norway"));
 
-        user2 = RetrieveUserDB.retrieveFromEmail("another@test.no");
+        user2 = RetrieveUserDB.retrieveFromEmail(user.getEmail());
 
         //here the first name is "ronny" because the user was updated and reserialised
         assertEquals(user2.getFirstName(), "ronny");
@@ -77,17 +74,79 @@ class DatabasePersistenceTest {
 
     @Test
     void assertsUserBasicDoesNotCreateUserProfile() {
+        User user = new User("basic@user.no", "ihavenoname");
 
+        String sql = "SELECT COUNT(*) AS amount FROM userProfiles WHERE user_fk = " + user.getId();
+
+        boolean ans = false;
+        try (Connection conn = ConnectDB.connect();
+             PreparedStatement str = conn.prepareStatement(sql)) {
+
+            ResultSet queryResult = str.executeQuery();
+            if (queryResult.getInt("amount") > 0) {
+                ans = true;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertFalse(ans);
     }
 
     @Test
     void assertsUserCreatesUserProfile() {
 
+
+
+        User user = new User("peter", "pepper", "1777", "pigletcollector",
+                "12341234123", "pete@pepp.er", "12341234",
+                new License("98 42 123456 1", LocalDate.parse("2011-11-22"),
+                        "Norway"));
+
+        String sql = "SELECT COUNT(*) AS amount FROM userProfiles WHERE user_fk = " + user.getId();
+
+        boolean ans = false;
+
+        try (Connection conn = ConnectDB.connect();
+             PreparedStatement str = conn.prepareStatement(sql)) {
+
+            ResultSet queryResult = str.executeQuery();
+            if (queryResult.getInt("amount") > 0) {
+                ans = true;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertTrue(ans);
     }
+
 
     @Test
     void assertsUserUpgradeCreatesUserProfile() {
+        User user = new User("basicbeforeupgrade@user.no", "ihavenoname");
 
+        String sql = "SELECT COUNT(*) AS amount FROM userProfiles WHERE user_fk = " + user.getId();
+
+        User user2 = new User("lorry", "dovid", "1777", user.getPassword(),
+                "12341234123", user.getEmail(), "12341234",
+                new License("98 44 123456 1", LocalDate.parse("2008-02-11"),
+                        "Norway"));
+
+        boolean ans = false;
+
+        try (Connection conn = ConnectDB.connect();
+             PreparedStatement str = conn.prepareStatement(sql)) {
+
+            ResultSet queryResult = str.executeQuery();
+            if (queryResult.getInt("amount") > 0) {
+                ans = true;
+            }
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+        assertTrue(ans);
     }
 
 }
