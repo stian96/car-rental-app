c50e31aa80012fffc6da30c617b9775ebdba5b81
rachel <rchlmd83@gmail.com>
2022-11-10 08:56:59 +0100

    made exit buttons in filtercar

--- java_project/src/main/java/com/example/java_project/FilterCarController.java
+++ java_project/src/main/java/com/example/java_project/FilterCarController.java
@@ -1,6 +1,47 @@
 package com.example.java_project;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.*;
+
+import java.io.IOException;
+
 public class FilterCarController {
+
+    @FXML
+    private RadioButton radioButton_manual;
+    @FXML
+    private RadioButton radioButton_automatic;
+    @FXML
+    private RadioButton radioButton_electric;
+    @FXML
+    private RadioButton radioButton_Gass;
+    @FXML
+    private RadioButton radioButton_Diesel;
+    @FXML
+    private ChoiceBox choiceBox_manufacturer;
+    @FXML
+    private Spinner spinner_seats;
+    @FXML
+    private TextField tf_yearModel;
+    @FXML
+    private TextField tf_gps;
+    @FXML
+    private TextField tf_priceClass;
+    @FXML
+    private Button button_exit;
+    @FXML
+    private Button button_search;
+
+    public void btExit(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("ToGoCar.fxml");
+    }
+    public void btSearch(ActionEvent event) throws IOException {
+        Main m = new Main();
+        m.changeScene("ToGoCar.fxml");
+    }
+
 
 
 }
