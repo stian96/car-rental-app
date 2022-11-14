23652bc4b452192fb61599f7c58b82e348881edd
a5h71 <ashtim@hiof.no>
2022-11-14 15:13:58 +0100

    changes to DetailedAdViewController and fxml

--- java_project/src/main/java/com/example/java_project/DetailedAdViewController.java
+++ java_project/src/main/java/com/example/java_project/DetailedAdViewController.java
@@ -1,55 +1,91 @@
 package com.example.java_project;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+
+import javafx.stage.Stage;
 import no.hiof.groupproject.models.Advertisement;
 import no.hiof.groupproject.models.RentOutAd;
+import no.hiof.groupproject.models.vehicle_types.Car;
 import no.hiof.groupproject.models.vehicle_types.Vehicle;
+import no.hiof.groupproject.tools.db.RetrieveAdvertisementDB;
+import no.hiof.groupproject.tools.db.RetrieveVehicleDB;
 
+import java.io.IOException;
+import java.net.URL;
 import java.time.LocalDate;
+import java.util.ResourceBundle;
 
 public class DetailedAdViewController {
     private RentOutAd selectedAd;
-    private LocalDate fromDate;
+    private Car vehicle;
+
+    private LocalDate fromDate, toDate;
+    private String stage = null;
 
     //labels with fx-Id's
     @FXML
-    private Label selectedOwner;
-    @FXML
-    private Label selectedMan;
-    @FXML
-    private Label selectedModel;
-    @FXML
-    private Label selectedModelYear;
-    @FXML
-    private Label selectedGearType;
-    @FXML
-    private Label selectedFuelType;
-    @FXML
-    private Label selectedSeatingCapacity;
-    @FXML
-    private Label selectedTowingCapacity;
+    private Label selectedOwner, selectedMan, selectedModel, selectedModelYear, selectedGearType,
+            selectedFuelType,
+            selectedSeatingCapacity,
+            selectedTowingCapacity;
+
+
     @FXML
     private Button button_back;
 
     // action Listen
 
 
-   @FXML private Label vehicleNameLabel, selectedFromDate;
-   @FXML private TextField tf_modelName;
+    @FXML
+    private Label vehicleNameLabel, selectedFromDate;
+    @FXML
+    private TextField tf_modelName;
 
-   public void fillData(RentOutAd roa){
-       selectedAd = roa;
-       tf_modelName.setText(roa.getVehicle().getModel());
-   }
+    public void fillData(RentOutAd roa) {
+        selectedAd = roa;
+        vehicle = (Car) roa.getVehicle();
+        selectedOwner.setText(roa.getUser().getFirstName());
+        selectedMan.setText(roa.getVehicle().getManufacturer());
+        selectedModel.setText(roa.getVehicle().getModel());
+        selectedModelYear.setText(String.valueOf(roa.getVehicle().getModelYear()));
+        selectedGearType.setText(roa.getVehicle().getGearType());
+        selectedFuelType.setText(roa.getVehicle().getEngineType());
+        selectedSeatingCapacity.setText(String.valueOf(vehicle.getSeatingCapacity()));
+        selectedTowingCapacity.setText(String.valueOf(vehicle.getTowingCapacity()));
+    }
 
-   public void adFromdate(LocalDate localDate){
-       fromDate = localDate;
-       selectedFromDate.setText(String.valueOf(fromDate));
+
+    public void changeSceneToFilterCar(ActionEvent event) throws IOException {
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("FilterCar.fxml"));
+        Parent pane = loader.load();
+
+        Scene scene = new Scene(pane);
+
+        //access the controller and call a method
 
 
-   }
+        //This line gets the Stage information
+        Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+
+        window.setScene(scene);
+        window.show();
+
+    }
+
+
+    public void setStage(Stage popupStage) {
+        this.stage = stage;
+    }
+
 
 }
