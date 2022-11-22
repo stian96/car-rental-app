402f36712c393450d3846237c3b0da84ce025f98
a5h71 <ashtim@hiof.no>
2022-11-22 13:10:15 +0100

    updated CreditDebitController

--- java_project/src/main/java/com/example/java_project/CreditDebitController.java
+++ java_project/src/main/java/com/example/java_project/CreditDebitController.java
@@ -3,6 +3,7 @@ package com.example.java_project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
@@ -20,9 +21,9 @@ public class CreditDebitController implements Initializable {
     @FXML
     private TextField tf_EmailAdd;
     @FXML
-    private TextField tf_Password,yearTxtfield,monthtxtfield;
+    private TextField tf_Password, yearTxtfield, monthtxtfield;
     @FXML
-    private Button button_addPayment;
+    private Button button_addPayment, backButton;
     @FXML
     private AnchorPane scenePane;
     @FXML
@@ -38,14 +39,18 @@ public class CreditDebitController implements Initializable {
         String CCV = tf_Password.getText();
         String month = monthtxtfield.getText();
         String year = yearTxtfield.getText();
-        CreditDebit c = new CreditDebit(cardNum,CCV,ChangeToInt(month),ChangeToInt(month));
-        if(!cardNum.isEmpty() || !CCV.isEmpty()
-        ||!month.isEmpty() || !year.isEmpty()){
-            try{
+        CreditDebit c = new CreditDebit(cardNum, CCV, ChangeToInt(month), ChangeToInt(month));
+        if (!cardNum.isEmpty() || !CCV.isEmpty()
+                || !month.isEmpty() || !year.isEmpty()) {
+            try {
                 InsertPaymentDB.insert(c);
-                BookingController.pay= c;
+                BookingController.pay = c;
 
                 button_addPayment.setText("Added!");
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("Payment is added!");
+                alert.show();
 
                 stage = (Stage) scenePane.getScene().getWindow();
                 stage.close();
@@ -54,17 +59,18 @@ public class CreditDebitController implements Initializable {
                 throw new RuntimeException(e);
             }
 
-        }else{emptyFieldWarning.setText("Fill out all fields");}
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Fill out all fields!");
+            alert.show();
+        }
 
     }
-    private int ChangeToInt(String value)
-    {
-        try
-        {
+
+    private int ChangeToInt(String value) {
+        try {
             return Integer.parseInt(value);
-        }
-        catch(Exception ex)
-        {
+        } catch (Exception ex) {
             return 0;
         }
     }
@@ -74,4 +80,10 @@ public class CreditDebitController implements Initializable {
         button_addPayment.setOnAction(this::btn_AddPayment);
     }
 
+    public void buttonStyle() {
+        backButton.setOnMouseEntered(e -> backButton.setStyle("-fx-background-color: #c9b502;"));
+        backButton.setOnMouseExited(e -> backButton.setStyle("-fx-background-color:  #f1c232;"));
+
+
+    }
 }
