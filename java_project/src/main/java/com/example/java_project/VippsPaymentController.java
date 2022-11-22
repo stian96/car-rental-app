3642e3b45bb6672bb4cbc8e0dd81e8efffb07535
a5h71 <ashtim@hiof.no>
2022-11-22 13:10:56 +0100

    updated VippsPaymentController

--- java_project/src/main/java/com/example/java_project/VippsPaymentController.java
+++ java_project/src/main/java/com/example/java_project/VippsPaymentController.java
@@ -3,11 +3,13 @@ package com.example.java_project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import no.hiof.groupproject.models.payment_methods.GooglePay;
+import no.hiof.groupproject.models.payment_methods.Paypal;
 import no.hiof.groupproject.models.payment_methods.Vipps;
 import no.hiof.groupproject.tools.db.InsertPaymentDB;
 
@@ -20,40 +22,57 @@ public class VippsPaymentController implements Initializable {
     @FXML
     private TextField tf_Password;
     @FXML
-    private Button button_addPayment;
+    private Button button_addPayment, backButton;
     @FXML
     private AnchorPane scenePane;
 
+    public static Paypal payment;
     Stage stage;
 
     @FXML
     public void btn_AddPayment(ActionEvent event) {
-        String tele = tf_EmailAdd.getText();
-        String pin= tf_Password.getText();
-        Vipps v = new Vipps(tele, pin);
-        if(!tele.isEmpty() && !pin.isEmpty()){
-            try{
+        String email = tf_EmailAdd.getText();
+        String password = tf_Password.getText();
+        Paypal p = new Paypal(email, password);
+        if (!email.isEmpty() && !password.isEmpty()) {
+            try {
+                InsertPaymentDB.insert(p);
+                BookingController.pay = p;
 
-                InsertPaymentDB.insert(v);
-                BookingController.pay = v;
                 button_addPayment.setText("Added!");
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("Payment is added!");
+                alert.show();
+
                 stage = (Stage) scenePane.getScene().getWindow();
                 stage.close();
 
-
-
-
-
             } catch (Exception e) {
                 throw new RuntimeException(e);
-            }
-
+            }}else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Fill out all fields!");
+            alert.show();
         }
 
+    }
+
+    public void backToBookingPage(ActionEvent event){
+        stage = (Stage) scenePane.getScene().getWindow();
+        stage.close();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         button_addPayment.setOnAction(this::btn_AddPayment);
+        backButton.setOnAction(this::backToBookingPage);
+        buttonStyle();
+    }
+
+    public void buttonStyle() {
+        backButton.setOnMouseEntered(e -> backButton.setStyle("-fx-background-color: #c9b502;"));
+        backButton.setOnMouseExited(e -> backButton.setStyle("-fx-background-color:  #f1c232;"));
+
     }
 }
