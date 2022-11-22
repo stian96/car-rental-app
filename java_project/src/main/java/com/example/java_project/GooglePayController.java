087858849db8b6b6aa445a025dfa6e69712f1f46
a5h71 <ashtim@hiof.no>
2022-11-22 13:19:38 +0100

    updated GooglePayController

--- java_project/src/main/java/com/example/java_project/GooglePayController.java
+++ java_project/src/main/java/com/example/java_project/GooglePayController.java
@@ -3,6 +3,7 @@ package com.example.java_project;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
@@ -21,7 +22,7 @@ public class GooglePayController implements Initializable {
     @FXML
     private TextField tf_Password;
     @FXML
-    private Button button_addPayment;
+    private Button button_addPayment, backButton;
     @FXML
     private AnchorPane scenePane;
 
@@ -30,34 +31,50 @@ public class GooglePayController implements Initializable {
 
     @FXML
     public void btn_AddPayment(ActionEvent event) {
-        Main m = new Main();
         String email = tf_EmailAdd.getText();
         String password = tf_Password.getText();
-        GooglePay p = new GooglePay(email,password);
-        if(!email.isEmpty() && !password.isEmpty()){
-            try{
-
+        GooglePay p = new GooglePay(email, password);
+        if (!email.isEmpty() && !password.isEmpty()) {
+            try {
                 InsertPaymentDB.insert(p);
                 BookingController.pay = p;
+
                 button_addPayment.setText("Added!");
+
+                Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                alert.setContentText("Payment is added!");
+                alert.show();
+
                 stage = (Stage) scenePane.getScene().getWindow();
                 stage.close();
 
-
-
-
-
             } catch (Exception e) {
                 throw new RuntimeException(e);
-            }
-
+            }}else{
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setContentText("Fill out all fields!");
+            alert.show();
         }
 
+    }
+
+    public void backToBookingPage(ActionEvent event){
+        stage = (Stage) scenePane.getScene().getWindow();
+        stage.close();
     }
 
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         button_addPayment.setOnAction(this::btn_AddPayment);
+        backButton.setOnAction(this::backToBookingPage);
+        buttonStyle();
     }
 
+    public void buttonStyle() {
+        backButton.setOnMouseEntered(e -> backButton.setStyle("-fx-background-color: #c9b502;"));
+        backButton.setOnMouseExited(e -> backButton.setStyle("-fx-background-color:  #f1c232;"));
+
+    }
 }
+
+
